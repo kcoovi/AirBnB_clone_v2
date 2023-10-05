5a8f3f6c0d49c095f1c71efed5c9a060f2ec66b9
Kevin COOVI <k.coovi@gmail.com>
2023-10-05 14:18:34 +0300

    AirBnb

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ from fabric.api import env
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["34.75.35.59", "3.238.29.119"]
+env.hosts = ["34.224.4.66", "35.153.50.145"]
 
 
 def do_deploy(archive_path):
